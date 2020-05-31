5333720113fd1a0e27fc148997d1287cea692204
Shittu <david.shittu@merrillcorp.com>
2020-05-31 02:43:12 -0500

    RN-001: add component logic

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const GoalItem = props => {
-    return(
-        <View style={styles.listItem}>
-            <Text>{props.title}</Text>
-        </View>
-    )
+const GoalItem = (props) => {
+  return (
+    <TouchableOpacity onLongPress={props.onDelete.bind (this, props.id)} >
+      <View key={props.title} style={styles.listItem}>
+        <Text>{props.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    listItem: {
-        padding: 10,
-        marginVertical: 10,
-        backgroundColor: "#ccc",
-        borderColor: "black",
-        borderWidth: 1,
-      }
-})
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 
 export default GoalItem;
